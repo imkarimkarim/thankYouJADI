ab946a1032214ee7efb47eb56ac3f26ba6113a7d
arianmonti <a.m.ontazeri1385@gmail.com>
2020-10-01 09:24:24 +0330

    add network

--- myScripts.js
+++ myScripts.js
@@ -34,6 +34,7 @@ $("document").ready(function () {
   img[13] = new Card("Python for beginer", "https://maktabkhooneh.org/course/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87-%D9%86%D9%88%DB%8C%D8%B3%DB%8C-%D8%A8%D8%A7-%D9%BE%D8%A7%DB%8C%D8%AA%D9%88%D9%86-%D9%85%D9%82%D8%AF%D9%85%D8%A7%D8%AA%DB%8C-mk346/", "py.jpg")
   img[14] = new Card("Advance python", "https://maktabkhooneh.org/course/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87-%D9%86%D9%88%DB%8C%D8%B3%DB%8C-%D8%A8%D8%A7-%D9%BE%D8%A7%DB%8C%D8%AA%D9%88%D9%86-%D9%BE%DB%8C%D8%B4%D8%B1%D9%81%D8%AA%D9%87-mk387/", "py2.jpg")
   img[15] = new Card("CEH", "https://maktabkhooneh.org/course/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D9%87%DA%A9-%D9%82%D8%A7%D9%86%D9%88%D9%86%D9%85%D9%86%D8%AF-CEH-mk641/", "ceh.jpg")
+  img[16] = new Card("Network", "https://www.youtube.com/watch?v=d5OWVGtZhJ4&list=PL-tKrPVkKKE00meXoxmIy6EgldK5XE-Z_", "network.jpg")
   book[1] = new Card("Just for fun", "http://linuxstory.ir/", "books.jpg")
   book[2] = new Card("Linux and life", "http://linuxbook.ir/", "books.jpg")
   book[3] = new Card("The lost bicycle guide for life", "http://bikezen.ir/", "books.jpg")
